a46c5e3f8ef159c3d39478a79f7f88fd795c0b21
Mario Júnior <mariojralves2006@gmail.com>
2024-03-18 16:36:20 -0300

    Configuração inicial

---
+++ src/main/java/com/cryptographyapi/cryptographyapi/CryptographyapiApplication.java
@@ -0,0 +1,13 @@
+package com.cryptographyapi.cryptographyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptographyapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptographyapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cryptographyapi/cryptographyapi/CryptographyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptographyapi.cryptographyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
